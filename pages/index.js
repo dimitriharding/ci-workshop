7e961404840d5c72982f2de1e44996edfc16438f
Dimitri Harding <vaughnharding@hotmail.com>
2021-10-29 15:25:21 -0500

    update welcome text

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 id="headerTitle" className={styles.title}>
-          Welcome to{" "}
-          <a href="https://nextjs.org"> NEW Sample Application - Update!</a>
+          Welcome to <a href="https://nextjs.org"> NEW Sample Application!</a>
         </h1>
 
         <p className={styles.description}>
